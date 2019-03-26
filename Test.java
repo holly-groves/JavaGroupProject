f77c205ba5a13ffea9458d328ff79d49f8c94a16
holly-groves <43141062+holly-groves@users.noreply.github.com>
2019-03-26 14:28:12 +0000

    UPADATED CODE- STILL NOT COMPLETE!!!!!!

---
+++ Test.java
@@ -0,0 +1,69 @@
+/**
+ * 
+ */
+
+/**
+ * @author Group 13
+ *
+ */
+public class Test {
+	
+	/**
+	 * Method to test creating the alphabet tree
+	 */
+	public void treeTest() 
+	{
+		System.out.println("TESTS\n");
+		testCreatingTree();
+		testDisplayingTreeEngToSpan();
+		testDisplayingTreeSpanToEng();
+	}
+	
+	/**
+	 * tests creating the alphabet tree
+	 */
+	public void testCreatingTree()
+	{
+		Tree test = new Tree();
+		System.out.println("Creating the alphabet tree");
+		test.createAlphabetTree();
+		System.out.println("\nIn-Order (alphabet)");
+		test.printInOrder(test.getRoot());
+		System.out.println("\nPre-Order (how the letters are put in to balance the tree");
+		test.printPreOrder(test.getRoot());
+		System.out.println("Test complete.");
+	}
+	
+	public void testDisplayingTreeEngToSpan()
+	{
+		Tree test = new Tree();
+		System.out.println("Displaying English to Spanish dictionary.....");
+		test.displayTreeEngToSpan(test.getRoot());		
+		System.out.println("Test complete.");
+		System.out.println();
+	}
+
+	public void testDisplayingTreeSpanToEng()
+	{
+		Tree test = new Tree();
+		System.out.println("Displaying Spanish to English dictionary.....");
+		test.displayTreeSpanToEng(test.getRoot());
+		System.out.println("Test complete.");
+		System.out.println();
+	}
+	
+	/**
+	 * Automated testing for; translate file, add and delete method.
+	 */
+	public void testTranslation()
+	{
+		Translation translation = new Translation();
+		System.out.println("Translate a file called test from english to spanish and rename it testTranslated.");
+		translation.testTranslateFileEngToSpan(); // will need a file called test.txt with a few random words on it.
+		System.out.println("\nTest add translation (add an english to spanish translation to the dictionary).");
+		translation.testAddEngToSpan();
+		System.out.println("\nDelete a translation (Delete an english to spanish translation called testWord and the translation testWordTranslated)");
+		translation.testDeleteEngToSpan();
+		System.out.println("Test complete.");
+	}
+}
