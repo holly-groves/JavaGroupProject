7ac483ff86ce2b1d14a63d423b476c4f4fbc2422
jesteggles <43212414+jesteggles@users.noreply.github.com>
2019-03-27 10:38:28 +0000

    The most recent versions of these classes.
    Use these for testing and shiz, I still have a few updates to some.

--- Menu.java
+++ Menu.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
  */
 public class Menu {
 
-	int addWords = 0;
 	Scanner s = new Scanner(System.in);
 	
 	/**
@@ -17,215 +16,214 @@ public class Menu {
 		Menu menu = new Menu();
 		menu.runMenu();
 	}
-	
-	/**
-	 * runs the menu for the program
-	 */
+
 	public void runMenu()
 	{
 		Translation tran = new Translation();
-		int choice = 0;
+		int lang = 0;
 		boolean exit = false;
 		
 		do
 		{
 			System.out.println("Tranlsator");
-			System.out.println("1. Translate a word");
-			System.out.println("2. Translate a phrase");
-			System.out.println("3. Translate from a file");
-			System.out.println("4. Search for a translation");
-			System.out.println("5. Add a translation");
-			System.out.println("6. Remove a translation");
-			System.out.println("7. Display dictonaries");
-			//option 8. is turning the add a translation setting on and off (Jack)
-			System.out.println("9. Run Automated Tests");
+			System.out.println("1. English to Spanish");
+			System.out.println("2. Spanish to English");
+			System.out.println("3. English to French");
+			System.out.println("4. French to English");
+			System.out.println("5. Run automated tests");
 			System.out.println("0. Exit");
 			
 			try
 			{
 				System.out.println("Enter menu choice: ");
-				choice = Integer.parseInt(s.nextLine());
+				lang = Integer.parseInt(s.nextLine());
 			}
 			catch (NumberFormatException e)
 			{
 				System.out.println("Not a valid menu choice, please enter a number");
 			}
 			
-			if (choice == 1) //translate a word
+			int option;
+			if (lang == 1) //eng to span
 			{
-				System.out.println("Translate a word"); //ADD TRANSLATION TIME (Beth)
+				option = getOption();
+				processOption(option, lang);
 				
-				int langChoice = getLang();
-				
-				String word = getWord();
-				String translation = "";
-				
-				if (langChoice == 1) //english to spanish
-				{
-					translation = tran.translateWord(1, word);
-				}
-				else if (langChoice == 2) //spanish to english
-				{
-					translation = tran.translateWord(2, word);
-				}
-				else //invalid
-				{
-					System.out.println("Invalid choice, enter again");
-					runMenu();
-				}
-				
-				if (translation != null)
-				{
-					System.out.println(word + " - " + translation);
-				}
-				else
-				{
-					System.out.println("No translation found");
-					//option to add a translation setting thing goes here (Jack)
-				}
 			}
-			else if (choice == 2) //translate a phrase
+			else if (lang == 2) //span to eng
 			{
-				//Michael
+				option = getOption();
+				processOption(option, lang);
 			}
-			else if (choice == 3) //translate from a file
+			else if (lang == 3) //eng to fren
 			{
-				int langChoice = getLang();
-				if (langChoice == 1)
-				{
-					tran.translateFileEngToSpan();
-				}
-				if (langChoice == 2)
-				{
-					tran.translateFileSpanToEng();
-				}
-				if (langChoice == 3)
-				{
-					tran.translateFileEngToFre();
-				}
-				if (langChoice == 4)
-				{
-					tran.translateFileFreToEng();
-				}
+				option = getOption();
+				processOption(option, lang);
 			}
-			else if (choice == 4) //search for a translation
+			else if (lang == 4) //fren to eng
 			{
-				String word = getWord();
-				int lang = getLang();
-				int ascii = getAscii(word);
-				
-				String search = tran.searchWord(lang, ascii, word);
-				
-				if (search == null) //word not found
-				{
-					System.out.println("Word not found in the dictonary");
-					//option to add a translation setting thing goes here (Jack)
-				}
-				else
-				{
-					System.out.println("Word has been found");
-					System.out.println(word + " - " + search);
-				}		
+				option = getOption();
+				processOption(option, lang);
 			}
-			else if (choice == 5) //add a translation
-			{
-				int langChoice = getLang();
-				if (langChoice == 1)
-				{
-					tran.addEngToSpan();
-				}
-				if (langChoice == 2)
-				{
-					tran.addSpanToEng();
-				}
-				if (langChoice == 3)
-				{
-					tran.addEngToFre();
-				}
-				if (langChoice == 4)
-				{
-					tran.addFreToEng();
-				}
-			}
-			else if (choice == 6) //remove a translation
-			{
-				int langChoice = getLang();
-				if (langChoice == 1)
-				{
-					tran.deleteEngToSpan();
-				}
-				if (langChoice == 2)
-				{
-					tran.deleteSpanToEng();
-				}
-				if (langChoice == 3)
-				{
-					tran.deleteEngToFre();
-				}
-				if (langChoice == 4)
-				{
-					tran.deleteFreToEng();
-				}
-			}
-			else if (choice == 7) //display dictionaries
-			{
-				Tree dictionary = new Tree();
-				dictionary.createAlphabetTree();
-				Scanner s = new Scanner(System.in);
-				
-				int i = getLang();
-				if (i == 1)
-				{
-					dictionary.displayTreeEngToSpan(dictionary.getRoot());
-				}
-				if (i == 2)
-				{
-					dictionary.displayTreeSpanToEng(dictionary.getRoot());
-				}
-				if (i == 3)
-				{
-					dictionary.displayTreeEngToFre(dictionary.getRoot());
-				}
-				if (i == 4)
-				{
-					dictionary.displayTreeFreToEng(dictionary.getRoot());
-				}
-			}
-			else if (choice == 8) //turn the add a translation setting thingy on or off
-			{
-				if (addWords == 1)
-				{
-					System.out.println("You have de-activated the auto add translation feature.");
-					addWords = 0;
-				} else if (addWords == 0)
-				{
-					System.out.println("You have activated the auto add translation feature.");
-					addWords = 1;
-				}
-			}
-			else if (choice == 9) //run automated tests
+			else if (lang == 5)
 			{
 				Test test = new Test();
-				System.out.println("Tests:"); //Tests need added (everyone)
-				test.treeTest();
+				test.runTest();
 			}
-			else if (choice == 0)
+			else if (lang == 0) //exit
 			{
 				System.out.println("Goodbye");
 				exit = true;
 			}
 			else
 			{
-				System.out.println("Invalid menu choice");
+				System.out.println("Invalid menu option");
 			}
-		}while (!exit);
+		}while(!exit);
 		System.exit(0);
 	}
+	
+	public int getOption()
+	{
+		int option = 0;
+		System.out.println("What would you like to do?");
+		System.out.println("1. Translate a word");
+		System.out.println("2. Translate a phrase");
+		System.out.println("3. Translate from a file");
+		System.out.println("4. Display dictonary");
+		System.out.println("5. Search for a translation");
+		System.out.println("6. Add a translation");
+		System.out.println("7. Remove a translation");
+		
+		try
+		{
+			System.out.println("Enter menu choice: ");
+			 option = Integer.parseInt(s.nextLine());
+		}
+		catch (NumberFormatException e)
+		{
+			System.out.println("Not a valid menu choice, please enter a number");
+		}
+		
+		return option;
+	}
 
-	/**
-	 * gets the word to be dealt with (translated/searched for/removed) from the user
-	 * @return String containing the user's word
-	 */
+	public void processOption(int opt, int lang)
+	{
+		Translation tran = new Translation();
+		if (opt == 1) //translate a word
+		{
+			String word = getWord();
+			String translation = tran.translateWord(lang, word);
+			System.out.println("Translation: " + translation);
+		}
+		else if (opt == 2) //translate a phrase
+		{
+			String translate = getPhrase();
+			tran.translatePhrase(lang, translate);
+		}
+		else if (opt == 3) //translate from file
+		{
+			if (lang == 1)
+			{
+				tran.translateFileEngToSpan(false, "blank", "blank");
+			}
+			if (lang == 2)
+			{
+				tran.translateFileSpanToEng(false, "blank", "blank");
+			}
+			if (lang == 3)
+			{
+				tran.translateFileEngToFre(false, "blank", "blank");
+			}
+			if (lang == 4)
+			{
+				tran.translateFileFreToEng(false, "blank", "blank");
+			}
+		}
+		else if (opt == 4) //display dictionary
+		{
+			Tree dictionary = new Tree();
+			dictionary.createAlphabetTree();
+			if (lang == 1)
+			{
+				dictionary.displayTreeEngToSpan(dictionary.getRoot());
+			}
+			if (lang == 2)
+			{
+				dictionary.displayTreeSpanToEng(dictionary.getRoot());
+			}
+			if (lang == 3)
+			{
+				dictionary.displayTreeEngToFre(dictionary.getRoot());
+			}
+			if (lang == 4)
+			{
+				dictionary.displayTreeFreToEng(dictionary.getRoot());
+			}
+		}
+		else if (opt == 5) //search for a translation
+		{
+			String word = getWord();
+			int ascii = getAscii(word);
+			
+			String search = tran.searchWord(lang, ascii, word);
+			
+			if (search == null) //word not found
+			{
+				System.out.println("Word not found in the dictonary");
+				//option to add a translation setting thing goes here (Jack)
+			}
+			else
+			{
+				System.out.println("Word has been found");
+				System.out.println(word + " - " + search);
+			}	
+		}
+		else if (opt == 6) //add a translation
+		{
+			if (lang == 1)
+			{
+				tran.addEngToSpan(false, "blank", "blank");
+			}
+			if (lang == 2)
+			{
+				tran.addSpanToEng(false, "blank", "blank");
+			}
+			if (lang == 3)
+			{
+				tran.addEngToFre(false, "blank", "blank");
+			}
+			if (lang == 4)
+			{
+				tran.addFreToEng(false, "blank", "blank");
+			}
+		}
+		else if (opt == 7) //remove a translation
+		{
+			if (lang == 1)
+			{
+				tran.deleteEngToSpan(false, "blank");
+			}
+			if (lang == 2)
+			{
+				tran.deleteSpanToEng(false, "blank");
+			}
+			if (lang == 3)
+			{
+				tran.deleteEngToFre(false, "blank");
+			}
+			if (lang == 4)
+			{
+				tran.deleteFreToEng(false, "blank");
+			}
+		}
+		else
+		{
+			System.out.println("Invalid choice");
+		}
+	}
+	
 	public String getWord()
 	{
 		System.out.println("Enter the word to translate: ");
@@ -233,26 +231,11 @@ public class Menu {
 		return word;
 	}
 	
-	/**
-	 * gets the language to be translated to from the user
-	 * @return int containing which language to use when translating
-	 */
-	public int getLang()
+	public String getPhrase()
 	{
-		int lang = 0;
-		System.out.println("1. English to Spanish");
-		System.out.println("2. Spanish to English");
-		System.out.println("Enter choice: ");
-		try
-		{
-			System.out.println("Enter menu choice: ");
-			lang = Integer.parseInt(s.nextLine());
-		}
-		catch (NumberFormatException e)
-		{
-			System.out.println("Not a valid menu choice, please enter a number");
-		}
-		return lang;
+		System.out.println("Enter the phrase to translate: ");
+		String phrase = s.nextLine();
+		return phrase;
 	}
 	
 	/**
@@ -267,5 +250,4 @@ public class Menu {
 		int  ascii = (int) letter; //gets the ascii value of the first letter
 		return ascii;
 	}
-	
 }
